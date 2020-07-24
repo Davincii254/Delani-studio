f2a0ac2d6149171d846adcb2ee2ed2a66183a39a
victor kuria <victorkuria7@gmail.com>
2020-07-24 13:48:19 +0300

    will add js code

---
+++ images/services_icons/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $("img#image1").click(function() {
+      $(".design-showing1").toggle();
+      $(".design-hidden1").toggle();
+    });
+    $("p#one").click(function() {
+      $(".design-showing1").toggle();
+      $(".design-hidden1").toggle();
+    });
+    $("img#image2").click(function() {
+      $(".design-showing2").toggle();
+      $(".design-hidden2").toggle();
+    });
+    $("p#two").click(function() {
+      $(".design-showing2").toggle();
+      $(".design-hidden2").toggle();
+    });
+    $("img#image3").click(function() {
+      $(".design-showing3").toggle();
+      $(".design-hidden3").toggle();
+    });
+    $("p#three").click(function() {
+        $(".design-showing3").toggle();
+        $(".design-hidden3").toggle();
+    });
+  });
